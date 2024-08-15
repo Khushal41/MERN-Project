83cf1d8114c06cf09cefebdfbe4ee3420fe1e68b
khushal41 <114830517+Khushal41@users.noreply.github.com>
2024-08-15 18:57:19 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-if (process.env.NODE_EW != "production") {
+if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
 
@@ -23,7 +22,6 @@ const userRouter = require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
-
 
 main()
     .then(() => {
@@ -52,7 +50,7 @@ const store = MongoStore.create({
     touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
     console.log("ERROR in MONGO session store", err);
 });
 
@@ -68,7 +66,6 @@ const sessionOptions = {
     },
 };
 
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -83,6 +80,11 @@ app.use((req, res, next) => {
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
+});
+
+// Add a route for the root path
+app.get("/", (req, res) => {
+    res.render("home"); // Assuming you have a "home.ejs" file in your "views" directory
 });
 
 app.use("/listings", listingsRouter);
@@ -102,3 +104,108 @@ app.use((err, req, res, next) => {
 app.listen(6969, () => {
     console.log("Server is listening on port 6969");
 });
+
+
+// if (process.env.NODE_EW != "production") {
+//     require('dotenv').config();
+// }
+
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const methodOverride = require("method-override");
+// const ejsMate = require("ejs-mate");
+// const ExpressError = require("./utils/ExpressError.js");
+// const session = require("express-session");
+// const MongoStore = require('connect-mongo');
+// const flash = require("connect-flash");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local").Strategy;
+// const User = require("./models/user.js");
+
+// const listingsRouter = require("./routes/listing.js");
+// const reviewsRouter = require("./routes/review.js");
+// const userRouter = require("./routes/user.js");
+
+// // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const dbUrl = process.env.ATLASDB_URL;
+
+
+// main()
+//     .then(() => {
+//         console.log("Connected to DB");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// async function main() {
+//     await mongoose.connect(dbUrl);
+// }
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
+// app.engine("ejs", ejsMate);
+// app.use(express.static(path.join(__dirname, "/public")));
+
+// const store = MongoStore.create({
+//     mongoUrl: dbUrl,
+//     crypto: {
+//         secret: process.env.SECRET,
+//     },
+//     touchAfter: 24 * 3600,
+// });
+
+// store.on("error", () => {
+//     console.log("ERROR in MONGO session store", err);
+// });
+
+// const sessionOptions = {
+//     store,
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+//         maxAge: 7 * 24 * 60 * 60 * 1000,
+//         httpOnly: true,
+//     },
+// };
+
+
+// app.use(session(sessionOptions));
+// app.use(flash());
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// app.use((req, res, next) => {
+//     res.locals.success = req.flash("success");
+//     res.locals.error = req.flash("error");
+//     res.locals.currUser = req.user;
+//     next();
+// });
+
+// app.use("/listings", listingsRouter);
+// app.use("/listings/:id/reviews", reviewsRouter);
+// app.use("/", userRouter);
+
+// app.all("*", (req, res, next) => {
+//     next(new ExpressError(404, "Page Not Found!!"));
+// });
+
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500, message = "Something Went Wrong!" } = err;
+//     res.status(statusCode).render("error.ejs", { message });
+// });
+
+// app.listen(6969, () => {
+//     console.log("Server is listening on port 6969");
+// });
