751643dfcc14b9b0f61be0d38ee2ef958653f328
khushal41 <114830517+Khushal41@users.noreply.github.com>
2024-08-16 11:13:15 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
-
-
-if (process.env.NODE_EMV != "production") {
+if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// const Listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+
+const dbUrl = process.env.ATLASDB_URL;
+
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
-
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("./models/user.js");
-
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL;
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// // const Listing = require("./models/listing.js")
+// const path = require("path");
+// const ejsMate = require("ejs-mate");
+// const methodOverride = require("method-override");
+// const ExpressError = require("./utils/ExpressError.js");
+// const session = require("express-session");
+// const MongoStore = require('connect-mongo');
+
+// const flash = require("connect-flash");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local").Strategy;
+// const User = require("./models/user.js");
+
+// const listingsRouter = require("./routes/listing.js");
+// const reviewsRouter = require("./routes/review.js");
+// const userRouter = require("./routes/user.js");
+
+// // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const dbUrl = process.env.ATLASDB_URL;
 
 
 main()
@@ -42,9 +60,9 @@ async function main() {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
@@ -82,9 +100,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    res.locals.currUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
     next();
 });
 
@@ -93,13 +111,12 @@ app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found!!"));
+    next(new ExpressError(404, "Page Not Found!"));
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Somthing went wrong!" } = err;
-    res.status(statusCode).render("error.ejs", { message })
+    const { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("error.ejs", { error: err });
 });
 
 app.listen(6969, () => {
